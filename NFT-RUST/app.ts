e359e8dd2dda74cbc5ae618642ff653c8d8e0839
ezrablackfish2 <ezrayeneneh1992@gmail.com>
2024-03-24 10:45:51 +0300

    solana minting

---
+++ NFT-RUST/app.ts
@@ -0,0 +1,115 @@
+import {
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    getAssociatedTokenAddress,
+    TOKEN_PROGRAM_ID,
+} from '@solana/spl-token';
+import {
+    Connection,
+    Keypair,
+    PublicKey,
+    SystemProgram,
+    SYSVAR_RENT_PUBKEY,
+    TransactionInstruction,
+    Transaction,
+    sendAndConfirmTransaction,
+} from '@solana/web3.js';
+import fs from 'mz/fs';
+import os from 'os';
+import path from 'path';
+import yaml from 'yaml';
+import {createKeypairFromFile} from './util';
+
+
+// Path to local Solana CLI config file.
+const CONFIG_FILE_PATH = path.resolve(
+    os.homedir(),
+    '.config',
+    'solana',
+    'cli',
+    'config.yml',
+);
+
+
+export async function main() {
+    const connection = new Connection("https://api.devnet.solana.com/", "confirmed");
+
+    console.log(`Successfully connected to Solana dev net.`);
+
+    const configYml = await fs.readFile(CONFIG_FILE_PATH, {encoding: 'utf8'});
+    const keypairPath = await yaml.parse(configYml).keypair_path;
+    const wallet = await createKeypairFromFile(keypairPath);
+    console.log(`Local account loaded successfully.`);
+
+    const programKeypair = await createKeypairFromFile(
+        path.join(
+            path.resolve(__dirname, './dist/program'), 
+            'mint-keypair.json'
+    ));
+    const programId = programKeypair.publicKey;
+
+    const mintKeyPair = Keypair.generate();
+    const tokenAddress = await getAssociatedTokenAddress(
+        mintKeyPair.publicKey,
+        wallet.publicKey
+      );
+
+      // Transact with our program
+
+      const instruction = new TransactionInstruction({
+        keys: [
+            // Mint account : all these are for variables (accounts) that are same as present in lib.rs at the starting of process_instruction fn
+            {
+                pubkey: mintKeyPair.publicKey,
+                isSigner: true,
+                isWritable: true,
+            },
+            // Token account : all these are for variables (accounts) that are same as present in lib.rs at the starting of process_instruction fn
+            {
+                pubkey: tokenAddress,
+                isSigner: false,
+                isWritable: true,
+            },
+            // Mint Authority : all these are for variables (accounts) that are same as present in lib.rs at the starting of process_instruction fn
+            {
+                pubkey: wallet.publicKey,
+                isSigner: true,
+                isWritable: false,
+            },
+            // Rent account : all these are for variables (accounts) that are same as present in lib.rs at the starting of process_instruction fn
+            {
+                pubkey: SYSVAR_RENT_PUBKEY,
+                isSigner: false,
+                isWritable: false,
+            },
+            // System program : all these are for variables (accounts) that are same as present in lib.rs at the starting of process_instruction fn
+            {
+                pubkey: SystemProgram.programId,
+                isSigner: false,
+                isWritable: false,
+            },
+            // Token program : all these are for variables (accounts) that are same as present in lib.rs at the starting of process_instruction fn
+            {
+                pubkey: TOKEN_PROGRAM_ID,
+                isSigner: false,
+                isWritable: false,
+            },
+            // Associated token program : all these are for variables (accounts) that are same as present in lib.rs at the starting of process_instruction fn
+            {
+                pubkey: ASSOCIATED_TOKEN_PROGRAM_ID,
+                isSigner: false,
+                isWritable: false,
+            },
+        ],
+        programId: programId,
+        data: Buffer.alloc(0),
+    })
+    // Further code !
+}
+
+main().then(
+    () => process.exit(),
+    err => {
+      console.error(err);
+      process.exit(-1);
+    },
+  );
---
+++ hashlips_art_engine/src/config.js
@@ -0,0 +1,150 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.sol;
+
+// General metadata for Solana
+const namePrefix = "Art of Eyes";
+const description = "NFTs diplaying various versions of eyes, built using stack of various layers of assets.";
+const baseUri = "ipfs://NewUriToReplace";
+
+
+const solanaMetadata = {
+  symbol: "AOE",
+  seller_fee_basis_points: 600, //Defines how much % you want from secondary market sales 1000 = 10%.
+  external_url: "",
+  creators: [
+    // Add the creators and their respective shares over here.
+    {
+      address: "hd8pVhXSRx3K5TEKus9PP3CkQ95E9UUcDmc6H9S7eFH",
+      share: 50,
+    },
+    {
+      address: "Dq9SmVjaRpH5Bec5n8KoZALcVgbqQAJ6SnGw6SUMuQCZ",
+      share: 50,
+    },
+  ],
+};
+
+
+// If you have selected Solana then the collection starts from 0 automatically.
+const layerConfigurations = [
+
+  // Write the code for specifying the order of stacking of the various layers of the NFTs here.
+  {
+    growEditionSizeTo: 15,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
+
+  {
+    growEditionSizeTo: 25,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Top lid" },
+      { name: "Bottom lid" },
+      { name: "Shine" },
+    ],
+  },
+
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+
+  // Specify the height and width of the NFTs
+  smoothing: false,
+  width: 600,
+  height: 600,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  // Mention the ratio of the pixels as given in the issue description.
+  
+  ratio: 3 / 128,
+  // The lower the number on the left, the more pixelated the image will be.
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
